a7be0c712d081b416fc8ab41ecc7200492fd09d3
Ryan Mahaffey <ryan.a.mahaffey@gmail.com>
2017-04-04 06:20:27 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,37 +1,59 @@
+# Blackjack mini game built in modular form. Yes you can actually win this one.
+
 def welcome
-  # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
+  rand (1..11)
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(total)
+  puts "Your cards add up to #{total}"
 end
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+  gets.chomp
 end
 
-def end_game
-  # code #end_game here
+def end_game(total)
+  puts "Sorry, you hit #{total}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
+  a = deal_card
+  b = deal_card
+  total = a + b
+  display_card_total(total)
+  total
 end
 
-def hit?
-  # code hit? here
+def hit?(total)
+  prompt_user
+  hit_or_stay = get_user_input
+  if hit_or_stay == 's'
+    total
+  elsif hit_or_stay == 'h'
+    total += deal_card
+  else
+    invalid_command
+    prompt_user
+  end
+  total
 end
 
 def invalid_command
-  # code invalid_command here
+  puts "Please enter a valid command"
+end
+
+def if_21(hand)
+  if hand == 21
+    puts "Blackjack! You got #{hand}!"
+  end
 end
 
 #####################################################
@@ -39,6 +61,15 @@ end
 #####################################################
 
 def runner
-  # code runner here
+  welcome
+  hand = initial_round
+  if_21(hand)
+  until hand >= 21
+    hand = hit?(hand)
+    display_card_total(hand)
+    if_21(hand)
+  end
+  if hand > 21
+    end_game(hand)
+  end
 end
-    
