f20719bb6d101d3d08c7743c0fa393de566c4ed8
Matthew Croak <mattcroak718@gmail.com>
2017-06-24 19:05:23 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,37 +1,60 @@
 def welcome
-  # code #welcome here
+    # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
   # code #deal_card here
+  1 + rand(11)
 end
 
-def display_card_total
+def display_card_total(total)
   # code #display_card_total here
+  puts "Your cards add up to #{total}"
 end
 
 def prompt_user
   # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
   # code #get_user_input here
+  gets.chomp
 end
 
-def end_game
+def end_game(total)
   # code #end_game here
+  puts "Sorry, you hit #{total}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
+  #code initial_round here
+  2.times {
+  deal_card
+  }
+  total = deal_card + deal_card
+  display_card_total(total)
+  total
 end
 
-def hit?
+def hit?(current_total)
   # code hit? here
+  prompt_user
+  input = get_user_input
+  if input == 'h'
+   current_total += deal_card
+  elsif input == 's'
+    current_total
+  else
+    invalid_command
+  end
 end
 
 def invalid_command
   # code invalid_command here
+  puts "Please enter a valid command."
+  prompt_user
 end
 
 #####################################################
@@ -40,5 +63,12 @@ end
 
 def runner
   # code runner here
+  welcome
+  total = initial_round #no need to call deal_card again since initial_round does it for us
+ # returns integer #this line both assigns a value, stores the value AND calls a method (initial_round)
+  until total > 21 do
+    total = hit?(total) # returns integer #important to put the new variable before the method. Again, here we are assigning a value, AND calling a method
+    display_card_total(total) #returns string
+  end
+  end_game(total)
 end
-    
