27ebef628ed0a953fd1b0fced57d9bd182a0836d
Matthew Garrett <mdgarrett1984@gmail.com>
2018-01-02 03:48:09 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -37,7 +37,18 @@ def initial_round
 end
 
 def hit?(card_total)
-
+    prompt_user
+    input = get_user_input
+    until input == 'h' || input == 's'
+      invalid_command
+      prompt_user
+      input = get_user_input
+    end
+    if input == 'h'
+      card_total += deal_card
+    elsif input == 's'
+      card_total
+  end
 end
 
 def invalid_command
@@ -51,4 +62,11 @@ end
 
 def runner
   # code runner here
+  welcome
+  card_total = initial_round
+  until card_total > 21
+    card_total = hit?(card_total)
+    display_card_total(card_total)
+  end
+  end_game(card_total)
 end
