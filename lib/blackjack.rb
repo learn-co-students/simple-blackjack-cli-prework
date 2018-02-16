3fafcb977ffda80de05a0e6ac1834d0a59b131e8
Justin Hamlin <justin.hamlin@gmail.com>
2018-02-16 23:19:40 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -15,11 +15,11 @@ def prompt_user
 end
 
 def get_user_input
-  # code #get_user_input here
+  gets.chomp
 end
 
-def end_game
-  # code #end_game here
+def end_game(card_total)
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
