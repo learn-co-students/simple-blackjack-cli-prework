42d6c8de95059767204c5903087cb7c9a487c5be
github username <github email address>
2018-04-25 23:05:48 -0700

    created #runner

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -46,10 +46,13 @@ def invalid_command
   prompt_user
 end
 
-#####################################################
-# get every test to pass before coding runner below #
-#####################################################
 
 def runner
-  # code runner here
+  welcome
+  card_total = initial_round
+  until card_total > 21
+    card_total = hit?(card_total)
+    display_card_total(card_total)
+  end
+end_game(card_total)
 end
