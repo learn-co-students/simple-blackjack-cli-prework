c415981df8f72b33cbd2fdb9181f7f7bd66d61eb
Camille Rheaume-Tremblay <camillerheaumet@hotmail.com>
2018-08-10 17:17:19 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -51,10 +51,10 @@ end
 
 def runner#(card_total)
   welcome
-  initial_round
+  card_total = initial_round
   until card_total > 21
-    hit?
-    display_card_total
+    card_total = hit?(card_total)
+    display_card_total(card_total)
   end
-  end_game
+  end_game(card_total)
 end
