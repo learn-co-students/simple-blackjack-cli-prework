e72831aaab5bc4b74c84127f90bda258e032228b
brianyu18 <brianyu1990@gmail.com>
2018-09-22 14:29:15 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,37 +1,53 @@
 def welcome
-  # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
+  rand(1..11)
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(card_total)
+  puts "Your cards add up to #{card_total}"
+  return card_total
 end
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+  gets.chomp
 end
 
-def end_game
-  # code #end_game here
+def end_game(taco_bell)
+  puts "Sorry, you hit #{taco_bell}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
+  card1 = deal_card
+  card2 = deal_card
+  total = card1 + card2
+  return display_card_total(total)
 end
 
-def hit?
-  # code hit? here
+def hit?(num)
+  new_total = 0
+  prompt_user
+  input = get_user_input
+  while input != 'h' && input != 's'
+    invalid_command
+    input = get_user_input
+  end
+  if input == 'h'
+    new_total = num + deal_card
+  else
+    new_total = num
+  end
+  new_total
 end
 
 def invalid_command
-  # code invalid_command here
+  puts "Please enter a valid command."
 end
 
 #####################################################
@@ -39,6 +55,27 @@ end
 #####################################################
 
 def runner
-  # code runner here
+  welcome
+  winner = initial_round
+  while winner < 21
+    winner = hit?(winner)
+  end
+  display_card_total(winner)
+  end_game(winner)
 end
     
+#     prompt_user
+#     input = get_user_input
+#     while input != 'h'
+#       if input == 's'
+#         prompt_user
+#         input = get_user_input
+#       else
+#         invalid_command
+#         input = get_user_input
+#       end
+#     end
+#     winner += deal_card
+#     winner
+#   end
+# end
