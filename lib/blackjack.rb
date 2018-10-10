fc16d80a70a55c1f72770847ce60eaf513384e4c
github username <github email address>
2018-10-09 20:06:51 -0400

    My first commit.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,33 +1,59 @@
+require 'pry'
+
 def welcome
   # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
   # code #deal_card here
+  rand(1..11)
 end
 
-def display_card_total
+def display_card_total(card_total)
   # code #display_card_total here
+  puts "Your cards add up to #{card_total}"
 end
 
 def prompt_user
   # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
   # code #get_user_input here
+  gets.chomp
 end
 
-def end_game
+def end_game(card_total)
   # code #end_game here
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
   # code #initial_round here
+  # first_card = deal_card
+  # second_card = deal_card
+  # sum_of_cards = first_card + second_card
+  # binding.pry 
+  sum_of_cards = deal_card + deal_card
+  display_card_total(sum_of_cards)
+  sum_of_cards
 end
 
-def hit?
+def hit?(card_total)
   # code hit? here
+  prompt_user
+  user_choice = get_user_input
+
+  if user_choice == "s"
+    card_total
+  elsif user_choice == "h"
+    card_total = card_total + deal_card
+  else  
+    card_total
+  end
+
 end
 
 def invalid_command
@@ -40,5 +66,13 @@ end
 
 def runner
   # code runner here
+  welcome
+  card_total = initial_round
+  until card_total > 21 do
+    card_total = hit?(card_total)
+    display_card_total(card_total)
+  end
+  # binding.pry 
+  end_game(card_total)
 end
     
