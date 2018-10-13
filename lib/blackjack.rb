a7f854f4e93b3b989819554ac26d14bbcbb5a0cb
github username <github email address>
2018-10-13 14:44:09 -0400

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -39,13 +39,12 @@ def hit?(current_card_value)
   user_input = get_user_input
   if user_input == "h"
     current_card_value += deal_card
+  elsif user_input == "s"
+    card_total
+  else
+    invalid_command
   end
   return current_card_value
-  #prompt_user
-  #if get_user_input == 'h'
-  #card_total += deal_card
-  #end
-  #card_total
 end
 
 def invalid_command
