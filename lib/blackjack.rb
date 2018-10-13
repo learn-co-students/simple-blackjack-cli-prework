62e46470364da9906beff47d03a7bb53d38f6e3c
github username <github email address>
2018-10-13 17:34:54 +0100

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,44 +1,65 @@
 def welcome
-  # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
+  card_total = rand(1..11)
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(num)
+puts "Your cards add up to #{num}"
 end
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+  gets
 end
 
-def end_game
-  # code #end_game here
+def end_game(n)
+  puts "Sorry, you hit #{n}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
+  cards = deal_card + deal_card
+  display_card_total(cards)
+  cards
+
+  end
+
+  def invalid_command
+    puts "Please enter a valid command"
+  end
+
+def hit?(current_total)
+  prompt_user
+  input = get_user_input
+    if input == "h"
+      current_total += deal_card
+    elsif input == "s"
+    current_total
+   else
+     invalid_command
+     prompt_user
+     input = get_user_input
+  end
 end
 
-def hit?
-  # code hit? here
-end
 
-def invalid_command
-  # code invalid_command here
-end
 
 #####################################################
 # get every test to pass before coding runner below #
 #####################################################
 
 def runner
-  # code runner here
+  welcome
+  hand = initial_round
+  until hand > 21
+    hand = hit?(hand)
+    display_card_total(hand)
+
+  end
+end_game(hand)
 end
-    
