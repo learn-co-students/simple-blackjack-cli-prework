adfcd99c4136a3736d2d222b802f7a8d25661eb9
camille feghali <camfeg@gmail.com>
2018-12-20 12:36:01 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,37 +1,63 @@
+require 'pry'
+
 def welcome
   # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
   # code #deal_card here
+  rand 1..11
 end
 
-def display_card_total
+def display_card_total(card_total)
   # code #display_card_total here
+  puts "Your cards add up to #{card_total}"
+  card_total
 end
 
 def prompt_user
   # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
   # code #get_user_input here
+  input = gets.chomp
+  input
 end
 
-def end_game
+def end_game(card_total)
   # code #end_game here
+  if card_total > 21
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
+end
 end
 
 def initial_round
   # code #initial_round here
+  card_one = deal_card
+  card_two = deal_card
+  display_card_total(card_one+card_two)
 end
 
-def hit?
-  # code hit? here
+def hit?(card_total)
+  prompt_user #ask the user if they want to hit or stay
+  user_input = get_user_input #set a variable to use for your if/else statement
+  if user_input == "h"
+    card_total += deal_card #increase your former total by the new number given by dealing another card
+  elsif user_input == "s"
+    card_total #return the number and prompt user to hit again
+  else
+    invalid_command
+  end
 end
 
 def invalid_command
   # code invalid_command here
+  puts "Please enter a valid command"
+  prompt_user
+  get_user_input
 end
 
 #####################################################
@@ -40,5 +66,12 @@ end
 
 def runner
   # code runner here
+  welcome
+  card_total = initial_round
+  until card_total >= 21
+
+    card_total = hit?(card_total)
+    display_card_total(card_total)
+  end
+end_game(card_total)
 end
-    
