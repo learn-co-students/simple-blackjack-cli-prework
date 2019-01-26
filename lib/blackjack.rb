7e881bae6f0debd07976d41dbe2dc505f0aea855
stephenyoon84 <stephenyoon84@gmail.com>
2019-01-26 13:30:23 -0500

    Simple-blackjack-cli - Done

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,37 +1,62 @@
 def welcome
   # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
   # code #deal_card here
+  return rand(1..11)
 end
 
-def display_card_total
+def display_card_total(card_total)
   # code #display_card_total here
+  puts "Your cards add up to #{card_total}"
 end
 
 def prompt_user
   # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
   # code #get_user_input here
+  # answer = gets.chomp
+  # return answer
+  gets.chomp
 end
 
-def end_game
+def end_game(card_total)
   # code #end_game here
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
   # code #initial_round here
+  card_sum =  deal_card + deal_card
+  display_card_total(card_sum)
+  return card_sum
 end
 
-def hit?
+def hit?(card_sum)
   # code hit? here
+  prompt_user
+  user_input = get_user_input
+
+  case user_input
+  when "s"
+    return card_sum
+  when "h"
+    card_sum = card_sum + deal_card
+    return card_sum
+  else
+    invalid_command
+    hit?(card_sum)
+  end
 end
 
 def invalid_command
   # code invalid_command here
+  puts "Please enter a valid command"
 end
 
 #####################################################
@@ -40,5 +65,12 @@ end
 
 def runner
   # code runner here
+  welcome
+  player_card = initial_round
+
+  until player_card > 21
+    player_card = hit?(player_card)
+    display_card_total(player_card)
+  end
+  end_game(player_card)
 end
-    
