f1a9834e8e5d43e2a67d22d31327cdf6ef80836b
Lisa Scott <lrscot@aol.com>
2019-02-04 20:05:16 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,44 +1,69 @@
 def welcome
-  # code #welcome here
+puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
+  rand(1..11)
   # code #deal_card here
 end
 
-def display_card_total
+def display_card_total(total)
+  puts "Your cards add up to #{total}"
   # code #display_card_total here
 end
 
 def prompt_user
+  puts "Type 'h' to hit or 's' to stay"
   # code #prompt_user here
 end
 
 def get_user_input
+  answer = gets.chomp
   # code #get_user_input here
 end
 
-def end_game
-  # code #end_game here
+def end_game(card_total)
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
+  sum = deal_card + deal_card
+    display_card_total(sum)
+    sum
 end
 
-def hit?
-  # code hit? here
+def hit(sum)?
+prompt_user_input
+user_input = get_user_input
+if user_input = 'h'
+  card_total += deal_card
+elsif user_input == "s"
+    card_total
+  else
+  invalid_command
+  end
 end
 
 def invalid_command
-  # code invalid_command here
-end
+  puts "Please enter a valid command"
+    prompt_user
+    get_user_inputend
 
 #####################################################
 # get every test to pass before coding runner below #
 #####################################################
 
 def runner
+  welcome #introduce the game
+  sleep 0.75
+  card_total = initial_round #determine your current total & tell user what number they currently have
+  until card_total > 21 #continue game until user loses
+    card_total = hit?(card_total)
+    display_card_total(card_total)
+  end
+  end_game(card_total) #prints out once user has lost
+end
+
+runner 
   # code runner here
 end
-    
