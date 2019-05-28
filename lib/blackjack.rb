c0c75381971a6d0af945f859879b0fe6ef468793
Dylan Harris <dylanharris23@gmail.com>
2019-05-28 22:47:44 +0000

    wip

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,29 +1,38 @@
 def welcome
   # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
   # code #deal_card here
+rand 1..11
 end
 
-def display_card_total
+def display_card_total(total)
   # code #display_card_total here
+  puts "Your cards add up to #{total}"
 end
 
 def prompt_user
   # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
   # code #get_user_input here
+ input = "h"
+ input = gets
 end
 
-def end_game
+def end_game(total)
   # code #end_game here
+  puts "Sorry, you hit #{total}. Thanks for playing!"
 end
 
 def initial_round
   # code #initial_round here
+  deal_card + deal_card
+
 end
 
 def hit?
@@ -41,4 +50,3 @@ end
 def runner
   # code runner here
 end
-    
